042f4b35a7cfa0bf6735a501146e6ce75e17f454
Hüveyda Tükle <huveydatukle@Huveydas-MacBook-Air-2.local>
2023-12-02 14:23:55 +0000

    Login modal created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
@@ -18,6 +18,4 @@ import { TripleInfoModuleComponent } from './triple-info-module/triple-info-modu
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'easy-exchange';
-}
+export class AppComponent {}
